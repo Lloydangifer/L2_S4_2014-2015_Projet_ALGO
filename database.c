803789e44e7e2685a957e8abd20ddc83d141c10e
Lloydangifer <vmanrique@netcourrier.com>
2015-02-16 12:29:52 +0100

    maj indentation

--- database.c
+++ database.c
@@ -6,21 +6,21 @@
 #define TELEPHONE_LENGTH 8
 
 struct directory_data {
-char last_name[NAME_LENGTH_MAX + 1];
-char first_name[NAME_LENGTH_MAX + 1];
-char telephone[TELEPHONE_LENGTH + 1];
+    char last_name[NAME_LENGTH_MAX + 1];
+    char first_name[NAME_LENGTH_MAX + 1];
+    char telephone[TELEPHONE_LENGTH + 1];
 };
 
 void directory_data_print(const struct directory_data *data){
-for(i=0;i<=NAME_LENGTH_MAX;++i){
-printf("%c",last_name[i]);
-}
-printf(" ");
-for(i=0;i<=NAME_LENGTH_MAX;++i){
-printf("%c",first_name[i]);
-}
-printf(": ");
-for(i=0;i<=TELEPHONE_LENGTH;++i){
-printf("%c",telephone[i]);
-}
+    for(i=0;i<=NAME_LENGTH_MAX;++i){
+        printf("%c",last_name[i]);
+    }
+    printf(" ");
+    for(i=0;i<=NAME_LENGTH_MAX;++i){
+        printf("%c",first_name[i]);
+    }
+    printf(": ");
+    for(i=0;i<=TELEPHONE_LENGTH;++i){
+        printf("%c",telephone[i]);
+    }
 }
