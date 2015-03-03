59b574ee1254d4c5eff0f27b9fe76bae2570cefb
Lloydangifer <vmanrique@netcourrier.com>
2015-03-03 17:25:54 +0100

    added main.c

--- directory_generate.c
+++ directory_generate.c
@@ -1,22 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <assert.h>
-
-#define NAME_LENGTH_MIN 3
-#define NAME_LENGTH_MAX 10
-#define TELEPHONE_LENGTH 8
-
-struct directory_data {
-    char last_name[NAME_LENGTH_MAX + 1];
-    char first_name[NAME_LENGTH_MAX + 1];
-    char telephone[TELEPHONE_LENGTH + 1];
-};
-struct directory {
-    struct directory_data **data;
-    size_t size;
-    size_t capacity;
-};
+#include "directory_generate.h"
+#include "main.c"
 
 void directory_data_print(const struct directory_data *data){
     assert(data);
@@ -213,6 +196,3 @@ void directory_add (struct directory *self, struct directory_data *data){
         directory_add(self,data);
     }
  }
-
-int main(int argc, char *argv[]){
-}
--- directory_generate.h
+++ directory_generate.h
@@ -10,12 +10,12 @@
 #define NAME_LENGTH_MAX 10
 #define TELEPHONE_LENGTH 8
 
-struct directory_data {
+struct directory_data{
     char last_name[NAME_LENGTH_MAX + 1];
     char first_name[NAME_LENGTH_MAX + 1];
     char telephone[TELEPHONE_LENGTH + 1];
 };
-struct directory {
+struct directory{
     struct directory_data **data;
     size_t size;
     size_t capacity;
@@ -23,3 +23,9 @@ struct directory {
 
 void directory_data_print(const struct directory_data *data);
 void directory_data_random(struct directory_data *data);
+void directory_create(struct directory *self);
+void directory_destroy(struct directory *self);
+void directory_add (struct directory *self, struct directory_data *data);
+void directory_random(struct directory *self, size_t n);
+
+#endif
